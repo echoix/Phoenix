0040ecb0ceb19e668671df28e62f5c3537e6fe5c
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-02-01 18:04:18 +0000

    from wx.svg._nanosvg cimport NSVGpaintType

--- wx/svg/_nanosvg.pyx
+++ wx/svg/_nanosvg.pyx
@@ -47,7 +47,7 @@ from cpython.buffer cimport (
     PyBuffer_Release)
 
 PY2 = sys.version_info[0] == 2
-from ._nanosvg cimport NSVGpaintType
+from wx.svg._nanosvg cimport NSVGpaintType
 #----------------------------------------------------------------------------
 # Replicate the C enums and values for Python, dropping the leading 'N'
 
